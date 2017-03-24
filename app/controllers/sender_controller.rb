4eb966ca914ab800da9d444200ba69ea3310505e
Matthias Vitte <matthias@zenchef.com>
2017-03-24 17:03:06 +0100

    adding a new sms called sms 3

--- app/controllers/sender_controller.rb
+++ app/controllers/sender_controller.rb
@@ -12,7 +12,6 @@ class SenderController < ApplicationController
     csm_infos = params["csm"].split(",")
     cst_name = csm_infos.first
     cst_number = csm_infos.last
-
     if phone.length.between? 10, 12
       add_region = phone.first.gsub(/["0"]/, '+33') + phone[1..11]
 
@@ -31,9 +30,15 @@ class SenderController < ApplicationController
         else
           api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, j’ai tenté de vous joindre pour la mise en place de votre service. Pouvez-vous me rappeler au #{cst_number} ? Merci. #{cst_name} de Zenchef", nil)
         end
+      when 3
+        if phone.first == "+"
+          api.call('sms.send', 'Zenchef', "#{phone}", "Bonjour, je suis votre nouveau contact chez Zenchef. Vous pouvez me contacter pour faire le point sur votre service au #{cst_number}. #{cst_name}", nil)
+        else
+          api.call('sms.send', 'Zenchef', "#{add_region}", "Bonjour, je suis votre nouveau contact chez Zenchef. Vous pouvez me contacter pour faire le point sur votre service au #{cst_number}. #{cst_name}", nil)
+        end
       end
 
-      flash[:success] = "SMS #{step_of_reconvering } envoyé à #{last_name} #{first_name}!"
+      flash[:success] = "SMS #{step_of_reconvering } envoyé à #{first_name.capitalize} #{last_name.upcase} !"
       redirect_to :root
     else
       flash[:error] = 'Le numéro est faux! il contient trop ou pas assez de caractères!'
